332b47c8d416c432111816e05f19f598b2f89680
Satria H R Harsono <hafizhharsono@gmail.com>
2018-01-15 05:42:14 +0700

    How to use bbs

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 from blocks.bbs import BBS
 
 
-def main():
+def main_bbs():
+    # BLUM BLUM SHUB
     # TODO create big prime number recommender
-    bbs = BBS(p=7, q=11, s=9)
+    p, q, s = 7, 11, 9
+    bbs = BBS(p, q, s)
+
     if not bbs.validated():
         exit()
-    print("BBS is ready")
+
+    first_number = bbs.next()
+    second_number = bbs.next()
+
+    print("{:^35}".format("BBS Keystream Generator"))
+    print("> Using p={}, q={}, s={}".format(p, q, s))
+    print("> {:8}number : {}".format("First", first_number))
+    print("> {:8}number : {}".format("Second", second_number))
 
 
 if __name__ == '__main__':
-    main()
+    main_bbs()
