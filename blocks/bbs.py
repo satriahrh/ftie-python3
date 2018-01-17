c9b961550fdb14093bfca212af9c182f1ce6f50f
Satria H R Harsono <hafizhharsono@gmail.com>
2018-01-17 12:20:01 +0700

    Refactor self.errors for security matter

--- blocks/bbs.py
+++ blocks/bbs.py
@@ -3,7 +3,7 @@ import suplementary.number_theory as nt
 
 class BBS:
     def __init__(self, p, q, s):
-        self.errors = {}
+        self.__errors = {}
         self.__validate(p, q, s)
         if self.validated():
             self.__m = p*q
@@ -12,28 +12,28 @@ class BBS:
     def get_errors(self, key=None):
         try:
             if key:
-                return self.errors[key]
-            return self.errors
+                return self.__errors[key]
+            return self.__errors
         except KeyError:
             return KeyError
 
     def __validate(self, p, q, s):
         self.__validated = False
         if p == q:
-            self.errors['validation'] = "p is equal to q"
+            self.__errors['validation'] = "p is equal to q"
             return
         if p % 4 != 3 or q % 4 != 3:
-            self.errors['validation'] = "p or q mod 4 are not 3"
+            self.__errors['validation'] = "p or q mod 4 are not 3"
             return
         if not nt.is_prime(p) or not nt.is_prime(q):
-            self.errors['validation'] = "p or q are not prime"
+            self.__errors['validation'] = "p or q are not prime"
             return
         if not (0 < s and s < p * q):
-            self.errors['validation'] = "s is to small or to big"
+            self.__errors['validation'] = "s is to small or to big"
             return
         import math
         if math.gcd(p * q, s) != 1:
-            self.errors['validation'] = "m and s are not relatively prime"
+            self.__errors['validation'] = "m and s are not relatively prime"
             return
         self.__validated = True
 
