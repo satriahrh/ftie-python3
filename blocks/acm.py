5ba46c3c2555b727ad3362f4efd1d0195b1fcad5
Satria H R Harsono <hafizhharsono@gmail.com>
2018-02-19 21:33:15 +0700

    Encryption and decryption

--- blocks/acm.py
+++ blocks/acm.py
@@ -19,9 +19,12 @@ class ACM:
             self.__a_inverse_matrix \
                 = np.linalg.inv(self.__a_matrix).astype(int).tolist()
             self.__maps_dimension = maps_dimension
+            self.__map = {}
+            self.__period = None
 
             if _a == 1 and _b == 1:
                 self.__type = 0
+                self.__period = self.__expect_period()
             elif _a == _b:
                 self.__type = 1
             else:
@@ -47,12 +50,41 @@ class ACM:
     def validated(self):
         return self.__validated
 
-    def encryption_map(self, number_of_iteration):
+    def __expect_period(self):
+        N = self.__maps_dimension
         if self.__type == 0:
-            return self.__mapping_zero(number_of_iteration)
-        return None
+            if N - 3 % 10 == 0 or N + 3 % 10 == 0:
+                return N + 1
+            elif N - 1 % 10 == 0 or N + 1 % 10 == 0:
+                return (N - 1) / 2
+        return 1
 
-    def __mapping_zero(self, number_of_iteration):
+    def __expect_number_of_iteration(self, number_of_iteration):
+        if number_of_iteration is None:
+            if self.__period is not None:
+                return self.__period - 1
+            return 1
+        return number_of_iteration
+
+    def get_map(self, number_of_iteration=None):
+        number_of_iteration \
+            = self.__expect_number_of_iteration(number_of_iteration)
+
+        try:
+            return self.__map[number_of_iteration]
+        except KeyError:
+            ret = None
+
+            if self.__type == 0:
+                ret = self.__mapping_zero(number_of_iteration)
+
+            self.__map[number_of_iteration] = ret
+            return ret
+
+    def __mapping_zero(self, number_of_iteration=None):
+        number_of_iteration \
+            = self.__expect_number_of_iteration(number_of_iteration)
+
         mapping = [
             [
                 [
@@ -70,3 +102,33 @@ class ACM:
         ]
 
         return mapping
+
+    def encrypt(self, matrix, number_of_iteration=None):
+        number_of_iteration \
+            = self.__expect_number_of_iteration(number_of_iteration)
+
+        maps = self.get_map(number_of_iteration)
+
+        ret = []
+        for x in range(self.__maps_dimension):
+            ret.append([])
+            for y in range(self.__maps_dimension):
+                xy = maps[x][y]
+                ret[x].append(matrix[xy[0]][xy[1]])
+
+        return ret
+
+    def decrypt(self, matrix, number_of_iteration=None):
+        maps = self.get_map(number_of_iteration)
+
+        ret = [
+            [None for y in range(self.__maps_dimension)]
+            for x in range(self.__maps_dimension)
+        ]
+
+        for x in range(self.__maps_dimension):
+            for y in range(self.__maps_dimension):
+                xy = maps[x][y]
+                ret[xy[0]][xy[1]] = matrix[x][y]
+
+        return ret
--- test/test_acm_discreete.py
+++ test/test_acm_discreete.py
@@ -4,19 +4,36 @@ from blocks.acm import ACM
 
 class TestAcmDiscreete(unittest.TestCase):
     def setUp(self):
-        self.__acm = ACM(1, 1, 11)
+        number_of_iteration = 7
+        self.__acm = ACM(1, 1, number_of_iteration)
         self.__original_map = [
             [
-                [x, y] for y in range(11)
-            ] for x in range(11)
+                [x, y] for y in range(number_of_iteration)
+            ] for x in range(number_of_iteration)
         ]
+        self.__plaintext = [
+            [
+                10 * x + y for y in range(number_of_iteration)
+            ] for x in range(number_of_iteration)
+        ]
+        self.__ciphertext = [
+            [
+                10 * x + y for y in range(number_of_iteration)
+            ] for x in range(number_of_iteration)
+        ]
+        self.__number_of_iteration = number_of_iteration
 
     def test_type_zero_period(self):
-        actual = self.__acm.encryption_map(5)  # 5 is the period of this form
+        actual = self.__acm.get_map(8)  # 5 is the period of this form
         expected = self.__original_map
         self.assertEqual(expected, actual)
 
     def test_type_zero_not_period(self):
-        actual = self.__acm.encryption_map(4)  # 4 is not the period of this form
+        actual = self.__acm.get_map()  # 4 is not the period of this form
         expected = self.__original_map
         self.assertNotEqual(expected, actual)
+
+    def test_encrypt_decrypt(self):
+        actual = self.__acm.decrypt(self.__acm.encrypt(self.__plaintext))
+        expected = self.__plaintext
+        self.assertEqual(actual, expected)
