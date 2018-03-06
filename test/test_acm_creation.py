448ba44914861ce7c93e46de39ffa98ed051b3f3
Satria H R Harsono <HafizhHarsono@gmail.com>
2018-03-06 16:16:58 +0700

    Correct way to call ACM

--- test/test_acm_creation.py
+++ test/test_acm_creation.py
@@ -7,7 +7,7 @@ class TestAcmValidation(unittest.TestCase):
     @unittest.expectedFailure
     def test_validation_a_and_b_is_good(self):
         try:
-            ACM(1, 1, 400)
+            ACM(_a=1, _b=1, number_of_iteration=400)
             self.fail('Validation is succeed and no errors')
         except ValidationError as validation_error:
             actual = validation_error.errors
@@ -16,7 +16,7 @@ class TestAcmValidation(unittest.TestCase):
 
     def test_validation_a_and_b_is_not_good(self):
         try:
-            ACM(0, 5, 400)
+            ACM(_a=0, _b=5, number_of_iteration=400)
             self.fail('Validation is succeed and no errors')
         except ValidationError as validation_error:
             actual = validation_error.errors
--- test/test_acm_discreete.py
+++ test/test_acm_discreete.py
@@ -16,19 +16,19 @@ class TestAcmDiscreete(unittest.TestCase):
         ]
 
     def test_type_zero_period(self):
-        acm = ACM(1, 1, 8)
+        acm = ACM(_a=1, _b=1, number_of_iteration=8)
         actual = acm.get_map(7)  # 8 is the period of this form
         expected = self.create_simple_map(7)
         self.assertEqual(expected, actual)
 
     def test_type_zero_not_period(self):
-        acm = ACM(1, 1, 1)
+        acm = ACM(_a=1, _b=1, number_of_iteration=1)
         actual = acm.get_map(7)  # 1 is not the period of this form
         expected = self.create_simple_map(7)
         self.assertNotEqual(expected, actual)
 
     def test_encrypt_decrypt(self):
-        acm = ACM(1, 1, 1)
+        acm = ACM(_a=1, _b=1, number_of_iteration=1)
         actual = acm.decrypt(acm.encrypt(self.create_simple_message(7)))
         expected = self.create_simple_message(7)
         self.assertEqual(actual, expected)
