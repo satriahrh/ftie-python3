3a0237a3d1ec378c5f88d5b7379f9bee205f95a7
Satria H R Harsono <HafizhHarsono@gmail.com>
2018-03-06 15:25:22 +0700

    Update test case for ACM

--- test/test_acm_creation.py
+++ test/test_acm_creation.py
@@ -1,22 +1,24 @@
 import unittest
 from blocks.acm import ACM
+from errors import ValidationError
 
 
 class TestAcmValidation(unittest.TestCase):
+    @unittest.expectedFailure
     def test_validation_a_and_b_is_good(self):
-        _a = 1
-        _b = 1
-        _n = 4000
-        acm = ACM(_a, _b, _n)
-        haystack = acm.get_errors('validation')
-        needle = "a or b is no more than 1"
-        self.assertNotIn(needle, haystack)
+        try:
+            ACM(1, 1, 400)
+            self.fail('Validation is succeed and no errors')
+        except ValidationError as validation_error:
+            actual = validation_error.errors
+            expected = "a or b is no more than 1"
+            self.assertEqual(expected, actual)
 
     def test_validation_a_and_b_is_not_good(self):
-        _a = 0
-        _b = 5
-        _n = 4000
-        acm = ACM(_a, _b, _n)
-        haystack = acm.get_errors('validation')
-        needle = "a or b is no more than 1"
-        self.assertIn(needle, haystack)
+        try:
+            ACM(0, 5, 400)
+            self.fail('Validation is succeed and no errors')
+        except ValidationError as validation_error:
+            actual = validation_error.errors
+            expected = "a or b is no more than 1"
+            self.assertEqual(expected, actual)
