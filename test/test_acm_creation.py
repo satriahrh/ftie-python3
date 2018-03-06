f4ce0137cc0bbbb318578b9ff5e15149a7c1dac1
Satria H R Harsono <HafizhHarsono@gmail.com>
2018-03-06 09:19:50 +0700

    Update test case to match newer scheme

--- test/test_acm_creation.py
+++ test/test_acm_creation.py
@@ -6,8 +6,8 @@ class TestAcmValidation(unittest.TestCase):
     def test_validation_a_and_b_is_good(self):
         _a = 1
         _b = 1
-        maps_dimension = 4000
-        acm = ACM(_a, _b, maps_dimension)
+        _n = 4000
+        acm = ACM(_a, _b, _n)
         haystack = acm.get_errors('validation')
         needle = "a or b is no more than 1"
         self.assertNotIn(needle, haystack)
@@ -15,8 +15,8 @@ class TestAcmValidation(unittest.TestCase):
     def test_validation_a_and_b_is_not_good(self):
         _a = 0
         _b = 5
-        maps_dimension = 4000
-        acm = ACM(_a, _b, maps_dimension)
+        _n = 4000
+        acm = ACM(_a, _b, _n)
         haystack = acm.get_errors('validation')
         needle = "a or b is no more than 1"
         self.assertIn(needle, haystack)
--- test/test_acm_discreete.py
+++ test/test_acm_discreete.py
@@ -3,36 +3,34 @@ from blocks.acm import ACM
 
 
 class TestAcmDiscreete(unittest.TestCase):
-    def setUp(self):
-        maps_dimension = 7
-        self.__acm = ACM(1, 1, maps_dimension)
-        self.__original_map = [
+    def create_simple_map(self, maps_dimension):
+        return [
             [
                 [x, y] for y in range(maps_dimension)
             ] for x in range(maps_dimension)
         ]
-        self.__plaintext = [
-            [
-                10 * x + y for y in range(maps_dimension)
-            ] for x in range(maps_dimension)
-        ]
-        self.__ciphertext = [
+
+    def create_simple_message(self, maps_dimension):
+        return [
             [
                 10 * x + y for y in range(maps_dimension)
             ] for x in range(maps_dimension)
         ]
 
     def test_type_zero_period(self):
-        actual = self.__acm.get_map(8)  # 8 is the period of this form
-        expected = self.__original_map
+        acm = ACM(1, 1, 8)
+        actual = acm.get_map(7)  # 8 is the period of this form
+        expected = self.create_simple_map(7)
         self.assertEqual(expected, actual)
 
     def test_type_zero_not_period(self):
-        actual = self.__acm.get_map()  # 7 is not the period of this form
-        expected = self.__original_map
+        acm = ACM(1, 1, 1)
+        actual = acm.get_map(7)  # 1 is not the period of this form
+        expected = self.create_simple_map(7)
         self.assertNotEqual(expected, actual)
 
     def test_encrypt_decrypt(self):
-        actual = self.__acm.decrypt(self.__acm.encrypt(self.__plaintext))
-        expected = self.__plaintext
+        acm = ACM(1, 1, 1)
+        actual = acm.decrypt(acm.encrypt(self.create_simple_message(7)))
+        expected = self.create_simple_message(7)
         self.assertEqual(actual, expected)
