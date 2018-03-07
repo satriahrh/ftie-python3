a4604e2a7f6d4b03348666cf01d78238c377054d
Satria H R Harsono <HafizhHarsono@gmail.com>
2018-03-07 11:28:36 +0700

    Update test case for constructor validation

--- test/test_rt_creation.py
+++ test/test_rt_creation.py
@@ -1,3 +1,27 @@
 import unittest
 from blocks.rt import RT
 from blocks.bbs import BBS
+from errors import ValidationError
+
+
+class TestRTConstructor(unittest.TestCase):
+    @unittest.expectedFailure
+    def test_validation_constructor_is_good(self):
+        try:
+            bbs = BBS(_p=5, _q=5, seed=3)
+            RT(bbs)
+            self.fail('Validation is succeed and no errors')
+        except ValidationError as err:
+            actual = err.errors
+            expected = "bbs is not BBS type"
+            self.assertEqual(expected, actual)
+
+    def test_validation_bbs_problem(self):
+        try:
+            bbs = None
+            RT(bbs)
+            self.fail('Validation is succeed and no errors')
+        except ValidationError as err:
+            actual = err.errors
+            expected = "bbs is not BBS type"
+            self.assertEqual(expected, actual)
