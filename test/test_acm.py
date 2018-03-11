68b21c0ed814d36d323ff05de2af4408950d4e1d
Satria H R Harsono <HafizhHarsono@gmail.com>
2018-03-12 01:34:43 +0700

    Unittests to be done

--- test/test_acm.py
+++ test/test_acm.py
@@ -33,3 +33,36 @@ class TestAcmDiscreete(unittest.TestCase):
         actual = acm.decrypt(ciphertext)
         expected = self.create_simple_message(7)
         self.assertEqual(actual, expected)
+
+
+class TestAcmGeneralEqual(unittest.TestCase):
+    def setUp(self):
+        self.create_simple_map = lambda maps_dimension: [
+            [
+                [x, y] for y in range(maps_dimension)
+            ] for x in range(maps_dimension)
+        ]
+        self.create_simple_message = lambda message_dimension: [
+            [
+                10 * x + y for y in range(message_dimension)
+            ] for x in range(message_dimension)
+        ]
+
+    def test_type_one_period(self):
+        acm = ACM(_a=3, _b=3, number_of_iteration=3)
+        actual = acm.get_map(9)  # 3 is the period of this form
+        expected = self.create_simple_map(9)
+        self.assertEqual(expected, actual)
+
+    def test_type_one_not_period(self):
+        acm = ACM(_a=3, _b=3, number_of_iteration=2)
+        actual = acm.get_map(9)  # 2 is not the period of this form
+        expected = self.create_simple_map(9)
+        self.assertNotEqual(expected, actual)
+
+    def test_encrypt_decrypt(self):
+        acm = ACM(_a=3, _b=3, number_of_iteration=1)
+        ciphertext = acm.encrypt(self.create_simple_message(9))
+        actual = acm.decrypt(ciphertext)
+        expected = self.create_simple_message(9)
+        self.assertEqual(actual, expected)
