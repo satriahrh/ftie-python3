f6dcd46f1890893c9f2b6e1acdcfa41c6f93f223
Satria H R Harsono <HafizhHarsono@gmail.com>
2018-03-12 01:00:03 +0700

    Remove uneeded code

--- blocks/transform.py
+++ blocks/transform.py
@@ -62,41 +62,6 @@ def decompile_pixels_to_bytes(pixels):
     return bts
 
 
-def numbers_to_pixels(numbers):
-    numbers = pad_numbers(numbers)
-
-    pixels = []
-    for i in range(int(len(numbers) / 3)):
-        pixels.append((
-            numbers[3 * i],
-            numbers[3 * i + 1],
-            numbers[3 * i + 2],
-        ))
-
-    return pixels
-
-
-def pixels_to_matrix(pixels, n_matrix=None):
-    from math import sqrt
-    len_pixels = None
-
-    if n_matrix is not None:
-        len_pixels = int(n_matrix ** 2)
-        pixels = pad_pixels(pixels, len_pixels)
-    else:
-        pixels = pad_pixels(pixels, len_pixels)
-        n_matrix = int(sqrt(len(pixels)))
-
-    matrix = [
-        [
-            pixels[n_matrix * x + y] for y in range(n_matrix)
-        ]
-        for x in range(n_matrix)
-    ]
-
-    return matrix
-
-
 def matrix_to_image(matrix):
     from PIL import Image
 
@@ -124,24 +89,6 @@ def image_to_matrix(image):
             matrix[x].append(image.getpixel((x, y)))
 
     return matrix
-
-
-def matrix_to_pixels(matrix):
-    pixels = []
-    for row in matrix:
-        for element in row:
-            pixels.append(element)
-
-    return pixels
-
-
-def pixels_to_numbers(pixels):
-    numbers = []
-    for pixel in pixels:
-        for number in pixel:
-            numbers.append(number)
-
-    return numbers
 
 
 def bytes_to_file(bts, file_path):
@@ -172,35 +119,6 @@ def pad_bytes(bts):
     bts += bytes(amount_of_padding)
 
     return bts
-
-
-def pad_numbers(numbers):
-    # to be processed in numbers_to_pixels
-    from math import ceil
-
-    # to make the original numbers do not get populated
-    numbers = numbers.copy()
-
-    expected_len_numbers = ceil(len(numbers) / 3) * 3
-
-    numbers += [0 for i in range(len(numbers), expected_len_numbers)]
-
-    return numbers
-
-
-def pad_pixels(pixels, expected_len_pixels=None):
-    # to be processed in pixels_to_matrix
-    from math import ceil, sqrt
-
-    # to make the original pixels do not get populated
-    pixels = pixels.copy()
-
-    if expected_len_pixels is None:
-        expected_len_pixels = int(pow(ceil(sqrt(len(pixels))), 2))
-
-    pixels += [(0, 0, 0) for i in range(len(pixels), expected_len_pixels)]
-
-    return pixels
 
 
 # STRIP
