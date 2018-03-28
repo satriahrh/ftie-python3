47fed97757fefc29f419fd06b9d179770a3f2262
Satria H R Harsono <HafizhHarsono@gmail.com>
2018-03-28 23:11:55 +0700

    Propose new performansi pengujian

--- pengujian_performansi.py
+++ pengujian_performansi.py
@@ -1,12 +1,20 @@
 from app import Application
 from memory_profiler import memory_usage
+from numpy import arange
+from os import path
 from time import time
+
+
+FROM = 1
+TO = 21
+STEP = 1
+MB_RANGE = arange(FROM, TO, STEP)
 
 
 def pengujian(p, q, s, a, b, n, mb, fungsi):
     print('==========================')
-    fstream = open('pengujian'+str(mb), 'wb')
-    fstream.write(b'1' * mb * 1000000)
+    fstream = open(path.join('build', 'pengujian'+str(mb)), 'wb')
+    fstream.write(b'1' * int(mb * 1000000))
     fstream.close()
     start = time()
     memory = memory_usage(
@@ -26,27 +34,27 @@ def pengujian(p, q, s, a, b, n, mb, fungsi):
 
 
 def encrypt(p, q, s, a, b, n, mb):
-    plainfile_fp = 'pengujian'+str(mb)
-    cipherimage_fp = 'pengujian'+str(mb)
+    plainfile_fp = path.join('build', 'pengujian'+str(mb))
+    cipherimage_fp = path.join('build', 'pengujian'+str(mb))
     Application(p, q, s, a, b, n).encrypt(plainfile_fp, cipherimage_fp)
 
 
 def decrypt(p, q, s, a, b, n, mb):
-    cipherimage_fp = 'pengujian'+str(mb)+'.bmp'
-    plainfile_fp = 'pengujian'+str(mb)
+    cipherimage_fp = path.join('build', 'pengujian'+str(mb)+'.bmp')
+    plainfile_fp = path.join('build', 'pengujian'+str(mb))
     Application(p, q, s, a, b, n).decrypt(cipherimage_fp, plainfile_fp)
 
 
 if __name__ == '__main__':
     print('PENGUJIAN ACM DISCRETE ==========================')
     print('ENKRIPSI ========================================')
-    for i in range(1, 21):
+    for i in MB_RANGE:
         pengujian(11, 7, 9, 1, 1, 100, i, encrypt)
 
     print()
 
     print('DEKRIPSI ========================================')
-    for i in range(1, 21):
+    for i in MB_RANGE:
         pengujian(11, 7, 9, 1, 1, 100, i, decrypt)
 
     print()
@@ -54,22 +62,22 @@ if __name__ == '__main__':
 
     print('PENGUJIAN ACM GENERAL a = b =====================')
     print('ENKRIPSI ========================================')
-    for i in range(1, 21):
+    for i in MB_RANGE:
         pengujian(11, 7, 9, 2, 2, 100, i, encrypt)
 
     print()
 
     print('DEKRIPSI ========================================')
-    for i in range(1, 21):
+    for i in MB_RANGE:
         pengujian(11, 7, 9, 2, 2, 100, i, decrypt)
 
     print('PENGUJIAN ACM GENERAL any a and b ===============')
     print('ENKRIPSI ========================================')
-    for i in range(1, 21):
+    for i in MB_RANGE:
         pengujian(11, 7, 9, 3, 4, 100, i, encrypt)
 
     print()
 
     print('DEKRIPSI ========================================')
-    for i in range(1, 21):
+    for i in MB_RANGE:
         pengujian(11, 7, 9, 3, 4, 100, i, decrypt)
